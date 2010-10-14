8ff04059c8bc9d5c827e7711520d1a16f08a0267
Joshua McKenty <jmckenty@gmail.com>
2010-10-14 17:12:55 +0200

    Updated tests

--- java_tests/org/opensha/commons/calc/CalcSuite.java
+++ java_tests/org/opensha/commons/calc/CalcSuite.java
@@ -5,20 +5,11 @@ import org.junit.runners.Suite;
 import org.opensha.commons.geo.LocationUtilsTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({
-	LocationUtilsTest.class,
-	TestFaultMomentCalc.class,
-	TestFunctionListCalc.class,
-	TestGaussianDistCalc.class,
-	TestFractileCurveCalculator.class,
-	TestMomentMagCalc.class
-})
+@Suite.SuiteClasses({ LocationUtilsTest.class, TestGaussianDistCalc.class,
+        TestMomentMagCalc.class })
+public class CalcSuite {
 
-public class CalcSuite
-{
-
-	public static void main(String args[])
-	{
-		org.junit.runner.JUnitCore.runClasses(CalcSuite.class);
-	}
+    public static void main(String args[]) {
+        org.junit.runner.JUnitCore.runClasses(CalcSuite.class);
+    }
 }
