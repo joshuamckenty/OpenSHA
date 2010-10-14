f9640eb79ed3f12dc7abd9628dcba13b805cf1d0
Joshua McKenty <jmckenty@gmail.com>
2010-10-14 17:11:24 +0200

    Moving test to java_tests and deleting scripts.

--- scripts/cron/restart_cruise.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dir=`dirname $0`
-${dir}/kill_cruise.sh
-${dir}/test_start_cc.sh
--- scripts/rmi/startHazardCurveCalcRMI_Server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/csh
-echo "THIS SCRIPT NEEDS TO BE UPDATED!"
-exit 1
-/usr/java/default/bin/java -Xmx500M -classpath /opt/install/apache-tomcat-5.5.20/webapps/OpenSHA/WEB-INF/lib/ERF.jar -Djava.rmi.server.codebase=file:/opt/install/apache-tomcat-5.5.20/webapps/OpenSHA/WEB-INF/lib/ERF.jar -Djava.security.policy=file:/opt/install/apache-tomcat-5.5.20/webapps/OpenSHA/HazardMapDatasets/SimpleRMI.policy -Djava.rmi.server.hostname=gravity.usc.edu org.opensha.sha.calc.remoteCalc.RegisterRemoteHazardCurveFactory &
